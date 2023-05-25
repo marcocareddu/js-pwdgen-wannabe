683356f0f32da26abfd56b7330b0ee2438d9a879
marcocareddu <saldumaccu@tuta.io>
2023-05-25 16:03:00 +0200

    Removed debug & var declaration

--- JS/javascript.js
+++ JS/javascript.js
@@ -1,1 +1,15 @@
-console.log('JS OK')
+// OPERAZIONI PRELILMINARI
+
+// Dichiaro le variabili
+const suffisso = '21';
+let pwd;
+let nome;
+let cognome;
+let colore_preferito;
+
+// Controllo su console
+console.log(suffisso);
+console.log(pwd);
+console.log(nome);
+console.log(cognome);
+console.log(colore_preferito);
